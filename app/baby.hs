e82f0db2e86e8ccd4fc6aeac05bd63e995becda9
Neil Grogan <neil@ngrogan.com>
2016-06-21 17:32:11 +0100

    Doing haskell exercises until chapter 2

---
+++ app/baby.hs
@@ -0,0 +1,11 @@
+doubleMe x = x + x
+-- doubleUs x y = x * 2 + y * 2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x*2
+
+-- This is a legal function name
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+-- Finds right angle triangles using list comprehension
+rightTriangles' = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c == 24]
